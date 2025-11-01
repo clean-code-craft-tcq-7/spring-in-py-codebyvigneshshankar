9b902f3ae9b8d416906e08310b3787e39ccf059e
Sudeep <sudeep.rp@gmail.com>
2025-11-01 23:31:59 +0530

    temperature

--- stats.test.py
+++ stats.test.py
@@ -4,11 +4,11 @@ import stats as statistics
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
-    computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
+    computedStats = statistics.calculateStats([98.6, 98.2, 97.8, 102.2])
     epsilon = 0.001
-    self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
-    self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
-    self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+    self.assertAlmostEqual(computedStats["avg"], 99.2, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 102.2, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 97.8, delta=epsilon)
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
